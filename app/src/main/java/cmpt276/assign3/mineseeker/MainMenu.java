4be885e77dab595447bdb8617d540506e3f020ad
Deborah Wang <dqw@sfu.ca>
2021-10-17 17:48:42 -0700

    Intent for switch activities added on MainMenu and WelcomeScreen.

--- app/src/main/java/cmpt276/assign3/mineseeker/MainMenu.java
+++ app/src/main/java/cmpt276/assign3/mineseeker/MainMenu.java
@@ -2,9 +2,12 @@ package cmpt276.assign3.mineseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import cmpt276.assign3.mineseeker.R;
+import cmpt276.assign3.mineseeker.ui.WelcomeScreen;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -12,5 +15,9 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static Intent makeIntent(Context context){
+        return new Intent(context, MainMenu.class);
     }
 }
