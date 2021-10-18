823c6af240e82fc9b23c4748dc39d0d82490e654
Deborah Wang <dqw@sfu.ca>
2021-10-17 17:40:32 -0700

    Basic text and button set up.

--- app/src/main/java/cmpt276/assign3/mineseeker/ui/WelcomeScreen.java
+++ app/src/main/java/cmpt276/assign3/mineseeker/ui/WelcomeScreen.java
@@ -3,6 +3,8 @@ package cmpt276.assign3.mineseeker.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cmpt276.assign3.mineseeker.R;
 
@@ -12,5 +14,16 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        setUpSkipWelcomeBtn();
     }
+
+    private void setUpSkipWelcomeBtn() {
+        Button skipWelcome = findViewById(R.id.welcome_SkipToMain);
+        skipWelcome.setOnClickListener(view -> {
+
+        });
+    }
+
+
 }
