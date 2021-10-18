c4fa326462fe90b5fa9b59d953083ea4a93435c9
Deborah Wang <dqw@sfu.ca>
2021-10-17 18:49:00 -0700

    Attributes and constructor set up.

--- app/src/main/java/cmpt276/assign3/mineseeker/Game.java
+++ app/src/main/java/cmpt276/assign3/mineseeker/Game.java
@@ -6,10 +6,24 @@ package cmpt276.assign3.mineseeker;
 * NEEDS TO BE UPDATED
 * */
 
+
+import cmpt276.assign3.mineseeker.model.GridObject;
+import cmpt276.assign3.mineseeker.model.Options;
+
 public class Game {
     private int rows, cols, numOfScans, numOfMines;
-    //private static Options instance;
-    public Game() {
+    private GridObject[][] grid;
+    private static Options options = Options.getInstance();
 
+    public Game() {
+        this.rows = options.getRows();
+        this.cols = options.getCols();
+        this.numOfMines = options.getNumOfMines();
+
+        this.numOfScans = 0;
+        grid = new GridObject[rows][cols];
     }
+
+
+
 }
