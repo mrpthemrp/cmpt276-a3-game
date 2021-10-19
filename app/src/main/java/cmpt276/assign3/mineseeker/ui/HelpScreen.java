4e3f3c6fb3069cff85e2243e4862e136e37d0bf7
Deborah Wang <dqw@sfu.ca>
2021-10-19 14:56:39 -0700

    Help Screen text finished, html also done.

--- app/src/main/java/cmpt276/assign3/mineseeker/ui/HelpScreen.java
+++ app/src/main/java/cmpt276/assign3/mineseeker/ui/HelpScreen.java
@@ -5,17 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ScrollView;
+import android.text.Html;
+import android.widget.TextView;
+import android.text.method.LinkMovementMethod;
 
 import cmpt276.assign3.mineseeker.R;
 
 public class HelpScreen extends AppCompatActivity {
+    private TextView hyperlink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_screen);
+
+        setupHyperlink();
+
+    }
+
+    private void setupHyperlink() {
+        hyperlink = findViewById(R.id.help_Hyperlink);
+        hyperlink.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 
