9dc6aa04a02e6064579348568b59c69066073174
Deborah Wang <dqw@sfu.ca>
2021-10-20 16:56:08 -0700

    Help screen UI done, all links work.

--- app/src/main/java/cmpt276/assign3/mineseeker/ui/HelpScreen.java
+++ app/src/main/java/cmpt276/assign3/mineseeker/ui/HelpScreen.java
@@ -18,14 +18,18 @@ public class HelpScreen extends AppCompatActivity {
         setContentView(R.layout.activity_help_screen);
 
         setupHyperlinks();
-
     }
 
     private void setupHyperlinks() {
         TextView hyperlink1 = findViewById(R.id.help_Hyperlink);
-        TextView hyperlink2 = findViewById(R.id.help_CitationContent);
+        TextView hyperlink2 = findViewById(R.id.help_CitationContent1);
+        TextView hyperlink3 = findViewById(R.id.help_CitationContent2);
+        TextView hyperlink4 = findViewById(R.id.help_CitationContent3);
+
         hyperlink1.setMovementMethod(LinkMovementMethod.getInstance());
         hyperlink2.setMovementMethod(LinkMovementMethod.getInstance());
+        hyperlink3.setMovementMethod(LinkMovementMethod.getInstance());
+        hyperlink4.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public static Intent makeIntent(Context context){
