5122705d9b6afab49a653298f7a88d2b372e034e
Deborah Wang <dqw@sfu.ca>
2021-10-20 00:14:22 -0700

    Images set.

--- app/src/main/java/cmpt276/assign3/mineseeker/ui/HelpScreen.java
+++ app/src/main/java/cmpt276/assign3/mineseeker/ui/HelpScreen.java
@@ -1,31 +1,31 @@
 package cmpt276.assign3.mineseeker.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
-import android.widget.TextView;
 import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import cmpt276.assign3.mineseeker.R;
 
 public class HelpScreen extends AppCompatActivity {
-    private TextView hyperlink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_screen);
 
-        setupHyperlink();
+        setupHyperlinks();
 
     }
 
-    private void setupHyperlink() {
-        hyperlink = findViewById(R.id.help_Hyperlink);
-        hyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+    private void setupHyperlinks() {
+        TextView hyperlink1 = findViewById(R.id.help_Hyperlink);
+        TextView hyperlink2 = findViewById(R.id.help_CitationContent);
+        hyperlink1.setMovementMethod(LinkMovementMethod.getInstance());
+        hyperlink2.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public static Intent makeIntent(Context context){
