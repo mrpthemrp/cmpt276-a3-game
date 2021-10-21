0d761c25cc61b54ab823c16f7091e9d87ce8fac8
Deborah Wang <dqw@sfu.ca>
2021-10-20 19:09:50 -0700

    Animation done.

--- app/src/main/java/cmpt276/assign3/mineseeker/ui/WelcomeScreen.java
+++ app/src/main/java/cmpt276/assign3/mineseeker/ui/WelcomeScreen.java
@@ -1,9 +1,11 @@
 package cmpt276.assign3.mineseeker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,6 +14,7 @@ import cmpt276.assign3.mineseeker.R;
 public class WelcomeScreen extends AppCompatActivity {
     private Timer timer = new Timer();
     private TimerTask task;
+    private ImageView carton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,19 @@ public class WelcomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_screen);
 
         setupTimerTask();
+        setupImageAnimation();
         setUpSkipWelcomeBtn();
         timer.schedule(this.task, 10000 );
         //delay goes by milliseconds, 10000 = 10 seconds
     }
+
+    private void setupImageAnimation() {
+        this.carton = findViewById(R.id.welcome_milkCarton);
+        this.carton.animate().setDuration(10000);
+        WelcomeScreen.this.carton.animate().rotationBy(360);
+        this.carton.animate().start();
+    }
+
 
     private void setupTimerTask() {
         this.task = new TimerTask() {
