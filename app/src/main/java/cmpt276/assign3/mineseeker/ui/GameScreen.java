90cf5a3bc76826dd6c828bfedb714ea675fe451e
Deborah Wang <dqw@sfu.ca>
2021-10-20 17:29:23 -0700

    Game screen UI done.

--- app/src/main/java/cmpt276/assign3/mineseeker/ui/GameScreen.java
+++ app/src/main/java/cmpt276/assign3/mineseeker/ui/GameScreen.java
@@ -5,15 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import cmpt276.assign3.mineseeker.R;
+import cmpt276.assign3.mineseeker.model.Options;
 
 public class GameScreen extends AppCompatActivity {
+    private Options options = Options.getInstance();
+    private int scans, numCartons, foundCartons;
+    private TextView numScans,numCartonsFound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        setupValues();
+    }
+
+    private void setupValues() {
+        this.scans = 0;
+        this.foundCartons = 0;
+        this.numCartons = options.getNumOfCartons();
+        this.numScans = findViewById(R.id.game_NumScans);
+        this.numCartonsFound = findViewById(R.id.game_FoundCartons);
+
+        this.numScans.setText(getString(R.string.game_NumberOfScans,this.scans));
+        this.numCartonsFound.setText(getString(R.string.game_FoundMilkCartons,this.foundCartons,this.numCartons));
     }
 
     public static Intent makeIntent(Context context){
