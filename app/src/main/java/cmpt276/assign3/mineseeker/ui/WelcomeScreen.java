ad277dcd319f2f6d5c4df86204469cf790934110
Deborah Wang <dqw@sfu.ca>
2021-10-20 18:16:09 -0700

    Timer is setup.

--- app/src/main/java/cmpt276/assign3/mineseeker/ui/WelcomeScreen.java
+++ app/src/main/java/cmpt276/assign3/mineseeker/ui/WelcomeScreen.java
@@ -1,30 +1,46 @@
 package cmpt276.assign3.mineseeker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cmpt276.assign3.mineseeker.R;
 
 public class WelcomeScreen extends AppCompatActivity {
+    private Timer timer = new Timer();
+    private TimerTask task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
+        setupTimerTask();
         setUpSkipWelcomeBtn();
+        timer.schedule(this.task, 10000 );
+        //delay goes by milliseconds, 10000 = 10 seconds
+    }
+
+    private void setupTimerTask() {
+        this.task = new TimerTask() {
+            @Override
+            public void run() {
+                endActivity();
+            }
+        };
     }
 
     private void setUpSkipWelcomeBtn() {
         Button skipWelcome = findViewById(R.id.welcome_SkipToMain);
-        skipWelcome.setOnClickListener(view -> {
-            Intent intent = MainMenu.makeIntent(WelcomeScreen.this);
-            startActivity(intent);
-            finish();
-        });
+        skipWelcome.setOnClickListener(view -> endActivity());
+    }
+
+    private void endActivity (){
+        Intent intent = MainMenu.makeIntent(WelcomeScreen.this);
+        startActivity(intent);
+        finish();
     }
 }
